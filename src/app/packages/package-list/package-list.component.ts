589b05fbc27869eab0fcd70d61275bcf2f67e14f
LeonardoGarciah <leonardoformulaum@Hotmail.com>
2021-10-28 20:11:15 -0300

    Feat Model package & Style pages

--- src/app/packages/package-list/package-list.component.ts
+++ src/app/packages/package-list/package-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from './package.model';
 
 @Component({
   selector: 'app-package-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackageListComponent implements OnInit {
 
+  packages: Package[] = [
+    new Package("Churrasco", "Churrasco de picanha", "https://pubimg.band.uol.com.br/files/1307321594d074a21c9f.png"),
+    new Package("Churrasco", "Churrasco de picanha", "https://pubimg.band.uol.com.br/files/1307321594d074a21c9f.png")
+  ];
   constructor() { }
 
   ngOnInit(): void {
