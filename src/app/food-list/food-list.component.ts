6df7cd3f1cfe195f22a0fbadf63fe823a7ac5bba
LeonardoGarciah <leonardoformulaum@Hotmail.com>
2021-10-28 20:24:59 -0300

    Model food

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Food } from '../shared/food.model';
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  foodList: Food[] = [
+    new Food("Carne",1),
+    new Food("Arroz",2),
+    new Food("Feijao",3),
+  ];
   constructor() { }
 
   ngOnInit(): void {
